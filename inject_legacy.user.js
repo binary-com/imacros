d6850601bf103662134eff2cdaecbafd7f8e8599
Amin <marashi.s.amin@gmail.com>
2016-02-25 14:31:56 +0330

    Some refactor and debug to the base code

--- inject_legacy.user.js
+++ inject_legacy.user.js
@@ -34,7 +34,7 @@
 		obj.css('height', '0px');
 	};
 
-	var removeAll = function removeAll() {
+	var removeAllHtml = function removeAllHtml() {
 		window.stop();
 		$(document)
 			.children()
@@ -49,13 +49,13 @@
 			.append('<title>Binary.com - Binary.com - Sharp Prices. Smart Trading.</title>');
 	};
 
-	var addNew = function addNew() {
+	var addDummyNewPage = function addDummyNewPage() {
 		$('body')
 			.append('<iframe style="border: 0px; position: fixed; left: 0px; top: 0px; height: 100%; width: 100%;" id="dummyNewPage" src="https://www.binary.com/trading?l=EN&dummyData=1"></iframe>');
 		dummyNewPage = $('#dummyNewPage');
 	};
 
-	var pageReady = function pageReady(condition, callback) {
+	var onPageReady = function onPageReady(condition, callback) {
 		var intervalID = setInterval(function () {
 			if (condition()) {
 				clearInterval(intervalID);
@@ -64,28 +64,28 @@
 		}, 500);
 	};
 
-	var addMockElements = function addMockElements() {
+	var addLegacyElements = function addLegacyElements() {
 		$('body')
 			.append('<div id="mockElements"></div>');
 		var mockElements = $('#mockElements');
 		mockElements
 			.append(GM_getResourceText('bet_container'));
 		hideElement(mockElements);
-		var bugged = false;
+		var faulty = false;
 		Object.keys(selectors)
 			.forEach(function (key) {
 				if ($(selectors[key])
 					.length !== 1) {
-					bugged = true;
+					faulty = true;
 					console.log(key);
 				}
 			});
-		if (!bugged) {
+		if (!faulty) {
 			console.log('All Mock Elements were added successfully');
 		}
 	};
 
-	var addClickBinding = function addClickBinding(legacySelector, newElement) {
+	var addClickRedirection = function addClickRedirection(legacySelector, newElement) {
 		$(legacySelector)
 			.click(function (event) {
 				event.preventDefault();
@@ -93,54 +93,64 @@
 			});
 	};
 
-	var addBinding = function addBinding(newElement, legacySelector, binding, mutationConfig, callback) {
-		if (newElement.length === 1) {
-			binding(legacySelector, newElement);
-			addObserver(newElement[0], mutationConfig, function (mutations) {
-				callback(mutations);
-			});
-		}
-	};
-
-	var addTwoWayBindings = function addTwoWayBindings() {
-		var mutationConfig = {
-			attributes: true,
-			attributeFilter: ['style'],
-		};
-		var purchase_button_top = $(dummyNewPage[0].contentDocument)
-			.find('#purchase_button_top');
-		var purchase_button_bottom = $(dummyNewPage[0].contentDocument)
-			.find('#purchase_button_bottom');
-		addBinding(purchase_button_top, selectors.btn_buybet_10, addClickBinding, mutationConfig, function (mutations) {
-			if ( purchase_button_top.css('display') === 'none' ) {
-				$('.price_box_first #bet_cal_buy').css('display', 'none');
-			} else {
-				$('.price_box_first #bet_cal_buy').css('display', 'block');
-			}
-		});
-		addBinding(purchase_button_bottom, selectors.btn_buybet_20, addClickBinding, mutationConfig, function (mutations) {
-			if ( purchase_button_bottom.css('display') === 'none' ) {
-				$('.price_box_last #bet_cal_buy').css('display', 'none');
-			} else {
-				$('.price_box_first #bet_cal_buy').css('display', 'block');
-			}
-		});
-	};
-
 	var addObserver = function addObserver(el, config, callback) {
 		var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+		callback();
 		var observer = new MutationObserver(function (mutations) {
 			callback(mutations);
 		});
 		observer.observe(el, config);
 	};
 
+	var mutationConfig = {
+		attributes: true,
+		attributeFilter: ['style'],
+	};
+
+	var bindings = [
+		function () {
+			var purchase_button_top = $(dummyNewPage[0].contentDocument)
+				.find('#purchase_button_top');
+			addClickRedirection(selectors.btn_buybet_10, purchase_button_top);
+			addObserver(purchase_button_top[0], mutationConfig, function (mutations) {
+				console.log('mutations');
+				if (purchase_button_top.css('display') === 'none') {
+					$('.price_box_first #bet_cal_buy')
+						.css('display', 'none');
+				} else {
+					$('.price_box_first #bet_cal_buy')
+						.css('display', 'block');
+				}
+			});
+		},
+		function () {
+			var purchase_button_bottom = $(dummyNewPage[0].contentDocument)
+				.find('#purchase_button_bottom');
+			addClickRedirection(selectors.btn_buybet_20, purchase_button_bottom);
+			addObserver(purchase_button_bottom[0], mutationConfig, function (mutations) {
+				if (purchase_button_bottom.css('display') === 'none') {
+					$('.price_box_last #bet_cal_buy')
+						.css('display', 'none');
+				} else {
+					$('.price_box_last #bet_cal_buy')
+						.css('display', 'block');
+				}
+			});
+		},
+	];
+
+	var addTwoWayBindings = function addTwoWayBindings() {
+		bindings.forEach(function (binding) {
+			binding();
+		});
+	};
+
 	var injectLegacyElements = function injectLegacyElements() {
-		addMockElements();
+		addLegacyElements();
 		addTwoWayBindings();
 	};
 
-	pageReady(function () {
+	onPageReady(function () {
 		var body = $('body');
 		if (body.length > 0) {
 			return true;
@@ -148,13 +158,13 @@
 			return false;
 		}
 	}, function () {
-		removeAll();
-		addNew();
+		removeAllHtml();
+		addDummyNewPage();
 
-		pageReady(function () {
-			var amount = dummyNewPage.contents()
-				.find('#amount');
-			if (amount.length > 0) {
+		onPageReady(function () {
+			var progress = dummyNewPage.contents()
+				.find('#trading_init_progress');
+			if (progress.css('display') === 'none') {
 				return true;
 			} else {
 				return false;

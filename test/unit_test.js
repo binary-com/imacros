16a077fb5c07f5818960da18953fabda12132c3b
Amin <marashi.s.amin@gmail.com>
2016-03-01 01:49:41 +0330

    Load jasmine specs at page load

--- test/unit_test.js
+++ test/unit_test.js
@@ -1,12 +1,31 @@
-describe("SUTE", function () {
-	it("EXPT", function () {
-		expect(true)
-			.toBe(true);
-	});
-	describe('OOP', function () {
-		it('IN', function () {
-			expect(0)
-				.toBe(1);
+var onReady = function onReady(condition, callback) {
+	var intervalID = setInterval(function () {
+		if (condition()) {
+			clearInterval(intervalID);
+			callback();
+		}
+	}, 500);
+};
+onReady(function () {
+	var progress = $('#dummyNewPage')
+		.contents()
+		.find('#trading_init_progress');
+	if (progress.length !== 0 && progress.css('display') === 'none') {
+		return true;
+	} else {
+		return false;
+	}
+}, function () {
+	describe("SUTE", function () {
+		it("EXPT", function () {
+			expect(true)
+				.toBe(true);
+		});
+		describe('OOP', function () {
+			it('IN', function () {
+				expect(0)
+					.toBe(1);
+			});
 		});
 	});
 });

0d365d05aa40682869dcec6396b2a948d0829c12
Amin <marashi.s.amin@gmail.com>
2016-03-01 00:42:41 +0330

    add global prefix for jasmineRequire

--- test/boot.js
+++ test/boot.js
@@ -9,85 +9,83 @@
  */
 
 (function (global) {
-	global.jasmineBoot = function jasmineBoot(jasmineRequire) {
 
-		/**
-		 * ## Require &amp; Instantiate
-		 *
-		 * Require Jasmine's core files. Specifically, this requires and attaches all of Jasmine's code to the `jasmine` reference.
-		 */
-		var jasmine = global.jasmine = jasmineRequire.core(jasmineRequire);
+	/**
+	 * ## Require &amp; Instantiate
+	 *
+	 * Require Jasmine's core files. Specifically, this requires and attaches all of Jasmine's code to the `jasmine` reference.
+	 */
+	var jasmine = global.jasmine = global.jasmineRequire.core(global.jasmineRequire);
 
-		/**
-		 * Create the Jasmine environment. This is used to run all specs in a project.
-		 */
-		var env = jasmine.getEnv();
+	/**
+	 * Create the Jasmine environment. This is used to run all specs in a project.
+	 */
+	var env = jasmine.getEnv();
 
-		/**
-		 * ## The Global Interface
-		 *
-		 * Build up the functions that will be exposed as the Jasmine public interface. A project can customize, rename or alias any of these functions as desired, provided the implementation remains unchanged.
-		 */
-		var jasmineInterface = jasmineRequire.interface(jasmine, env);
+	/**
+	 * ## The Global Interface
+	 *
+	 * Build up the functions that will be exposed as the Jasmine public interface. A project can customize, rename or alias any of these functions as desired, provided the implementation remains unchanged.
+	 */
+	var jasmineInterface = global.jasmineRequire.interface(jasmine, env);
 
-		/**
-		 * Add all of the Jasmine global/public interface to the global scope, so a project can use the public interface directly. For example, calling `describe` in specs instead of `jasmine.getEnv().describe`.
-		 */
-		extend(global, jasmineInterface);
-
-
-
-		var reporter = {
+	/**
+	 * Add all of the Jasmine global/public interface to the global scope, so a project can use the public interface directly. For example, calling `describe` in specs instead of `jasmine.getEnv().describe`.
+	 */
+	extend(global, jasmineInterface);
 
 
 
-
-			jasmineStarted: function (suiteInfo) {
-
-
-				console.log('Running suite with ' + suiteInfo.totalSpecsDefined);
-			},
-			suiteStarted: function (result) {
-				console.log('Suite started: ' + result.description + ' whose full description is: ' + result.fullName);
-			},
-			specStarted: function (result) {
-				console.log('Spec started: ' + result.description + ' whose full description is: ' + result.fullName);
-			},
+	var reporter = {
 
 
-			specDone: function (result) {
-
-				console.log('Spec: ' + result.description + ' was ' + result.status);
-				for (var i = 0; i < result.failedExpectations.length; i++) {
-					console.log('Failure: ' + result.failedExpectations[i].message);
-					console.log(result.failedExpectations[i].stack);
-				}
-				console.log(result.passedExpectations.length);
-			},
-			suiteDone: function (result) {
 
 
-				console.log('Suite: ' + result.description + ' was ' + result.status);
-				for (var i = 0; i < result.failedExpectations.length; i++) {
+		jasmineStarted: function (suiteInfo) {
 
-					console.log('AfterAll ' + result.failedExpectations[i].message);
-					console.log(result.failedExpectations[i].stack);
-				}
-			},
-			jasmineDone: function () {
-				console.log('Finished suite');
+
+			console.log('Running suite with ' + suiteInfo.totalSpecsDefined);
+		},
+		suiteStarted: function (result) {
+			console.log('Suite started: ' + result.description + ' whose full description is: ' + result.fullName);
+		},
+		specStarted: function (result) {
+			console.log('Spec started: ' + result.description + ' whose full description is: ' + result.fullName);
+		},
+
+
+		specDone: function (result) {
+
+			console.log('Spec: ' + result.description + ' was ' + result.status);
+			for (var i = 0; i < result.failedExpectations.length; i++) {
+				console.log('Failure: ' + result.failedExpectations[i].message);
+				console.log(result.failedExpectations[i].stack);
 			}
-		};
+			console.log(result.passedExpectations.length);
+		},
+		suiteDone: function (result) {
 
 
-		env.addReporter(reporter);
+			console.log('Suite: ' + result.description + ' was ' + result.status);
+			for (var i = 0; i < result.failedExpectations.length; i++) {
 
-		env.execute();
-
-		function extend(destination, source) {
-			for (var property in source) destination[property] = source[property];
-			return destination;
+				console.log('AfterAll ' + result.failedExpectations[i].message);
+				console.log(result.failedExpectations[i].stack);
+			}
+		},
+		jasmineDone: function () {
+			console.log('Finished suite');
 		}
-
 	};
+
+
+	env.addReporter(reporter);
+
+	env.execute();
+
+	function extend(destination, source) {
+		for (var property in source) destination[property] = source[property];
+		return destination;
+	}
+
 })(this);
